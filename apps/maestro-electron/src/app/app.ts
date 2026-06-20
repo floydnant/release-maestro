cbbec83b6a72723f2162f67f1b5ca1d63abd494e
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2026-06-20 22:45:45 +0200

    refactor: clean up tsconfigs

--- apps/maestro-electron/src/app/app.ts
+++ apps/maestro-electron/src/app/app.ts
@@ -136,7 +136,7 @@ export default class App {
         } else {
             App.mainWindow?.loadURL(
                 format({
-                    pathname: join(__dirname, rendererAppName, 'index.html'),
+                    pathname: join(__dirname, '..', rendererAppName, 'index.html'),
                     protocol: 'file:',
                     slashes: true,
                 }),
